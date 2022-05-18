2098ae54dd218780eccc6f9c275e52516cab27b5
Daniel THIRION <defvs.daniel@gmail.com>
2022-05-18 13:02:05 +0000

    Create program files

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Arduino.h"
+#include "HID-Project.h"
+#include "settings.h"
+
+void setup() {
+    Gamepad.begin();
+}
+
+void loop() {
+    uint8_t portb, portc, portd, porte;
+}
---
+++ src/settings.h
@@ -0,0 +1,12 @@
+// Pins
+#define BT_LEFT_R 1
+#define BT_LEFT_G 2
+#define BT_LEFT_B 3
+#define BT_RIGHT_R 4
+#define BT_RIGHT_G 5
+#define BT_RIGHT_B 6
+#define BT_LEFT_START 7
+#define BT_RIGHT_START 8
+#define BT_MACRO_1 9
+#define BT_MACRO_2 10
+#define SLIDER 11
